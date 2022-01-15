1b910dd7c4821947fed1262029fa3eb1259bbd98
Jan von der Assen <jan.vda@gmx.ch>
2022-01-15 16:50:58 +0100

    darn trailing spaces

--- src/fuenfer-ext.js
+++ src/fuenfer-ext.js
@@ -1,4 +1,4 @@
-// This is a subset of the dataset published by Marvin Wendt 
+// This is a subset of the dataset published by Marvin Wendt
 // https://gist.github.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4
 export default [
   'aalen',
